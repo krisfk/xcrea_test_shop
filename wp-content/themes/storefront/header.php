73e6ccdb4dcf4cd6503c87b936881998dbf79f2c
kaiyu li <krisfk@gmail.com>
2021-02-08 20:08:00 +0800

    .

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -7,29 +7,31 @@
  * @package storefront
  */
 
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+    test
+    <?php wp_body_open(); ?>
 
-<?php wp_body_open(); ?>
+    <?php do_action( 'storefront_before_site' ); ?>
 
-<?php do_action( 'storefront_before_site' ); ?>
+    <div id="page" class="hfeed site">
+        <?php do_action( 'storefront_before_header' ); ?>
 
-<div id="page" class="hfeed site">
-	<?php do_action( 'storefront_before_header' ); ?>
+        <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 
-	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
-
-		<?php
+            <?php
 		/**
 		 * Functions hooked into storefront_header action
 		 *
@@ -48,9 +50,9 @@
 		do_action( 'storefront_header' );
 		?>
 
-	</header><!-- #masthead -->
+        </header><!-- #masthead -->
 
-	<?php
+        <?php
 	/**
 	 * Functions hooked in to storefront_before_content
 	 *
@@ -60,8 +62,8 @@
 	do_action( 'storefront_before_content' );
 	?>
 
-	<div id="content" class="site-content" tabindex="-1">
-		<div class="col-full">
+        <div id="content" class="site-content" tabindex="-1">
+            <div class="col-full">
 
-		<?php
+                <?php
 		do_action( 'storefront_content_top' );
