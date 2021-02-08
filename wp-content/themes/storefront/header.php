3e6ae886f799d927e0070516b0ae21ae116edefa
kaiyu li <krisfk@gmail.com>
2021-02-08 20:21:13 +0800

    .

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -27,10 +27,10 @@ $user_ID = get_current_user_id();
 // echo $user_ID;
 // echo 9999;
 // the_field('credit', 'user_'. $user_ID);
-// $author_field = get_field('credit', 'user_'. $user_ID );
-// echo $author_field;
+$author_field = get_field('credit', 'user_'. $user_ID );
+echo $author_field;
 
-echo 888;
+// echo 888;
 
 ?>
 
